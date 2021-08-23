03ee9e6f761fca751a552ea1561075c55412c328
Victor Hristov <Victor.h7@live.com>
2021-08-23 20:44:26 +0200

    Finished the 9th video

--- src/App.js
+++ src/App.js
@@ -1,55 +1,71 @@
 import Header from "./Header.js";
+import SearchItem from "./SearchItem.js";
+import AddItem from "./AddItem.js";
 import Content from "./Content.js";
 import Footer from "./Footer.js";
 
 import { useState } from "react";
 
 function App() {
-  const [items, setItems] = useState([
-    {
-      id: 1,
-      checked: true,
-      item: "One half pound bag of cocoa covered almonds unsalted",
-    },
-    {
-      id: 2,
-      checked: false,
-      item: "Item 2",
-    },
-    {
-      id: 3,
-      checked: false,
-      item: "Item 3",
-    },
-  ]);
+  // State
+  const [items, setItems] = useState(
+    JSON.parse(localStorage.getItem("Shoppinglist"))
+  );
+  const [newItem, setNewItem] = useState("");
+  const [search, setSearch] = useState("");
+
+  const setAndSaveItems = (newItems) => {
+    setItems(newItems);
+    localStorage.setItem("Shoppinglist", JSON.stringify(newItems));
+  };
+
+  const addItem = (item) => {
+    const id = items.length ? items[items.length - 1].id + 1 : 1;
+    const myNewItem = { id, checked: false, item };
+    const listItems = [...items, myNewItem];
+
+    setAndSaveItems(listItems);
+  };
 
   const handleCheck = (id) => {
     const listItems = items.map((item) =>
       item.id === id ? { ...item, checked: !item.checked } : item
     );
-
-    setItems(listItems);
-
-    localStorage.setItem("Shoppinglist", JSON.stringify(listItems));
+    setAndSaveItems(listItems);
 
     console.log(`key: ${id}`);
   };
 
   const handleDelete = (id) => {
     const listItems = items.filter((item) => item.id !== id);
-
-    setItems(listItems);
-
-    localStorage.setItem("Shoppinglist", JSON.stringify(listItems));
+    setAndSaveItems(listItems);
 
     console.log(id);
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    if (!newItem) return;
+    // addItem func
+    addItem(newItem);
+    setNewItem("");
+
+    console.log(`Submitted: ${newItem}`);
   };
 
   return (
     <div className="App">
       <Header title="Grocery list" />
+      <AddItem
+        newItem={newItem}
+        setNewItem={setNewItem}
+        handleSubmit={handleSubmit}
+      />
+      <SearchItem search={search} setSearch={setSearch} />
       <Content
-        items={items}
+        items={items.filter((item) =>
+          item.item.toLowerCase().includes(search.toLowerCase())
+        )}
         handleCheck={handleCheck}
         handleDelete={handleDelete}
       />
